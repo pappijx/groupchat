e28f51026e4d53458d24efab26e4f50f48b6a83e
pappijx <ayush.papani123@gmail.com>
2022-01-20 02:58:59 +0530

    chat addons

--- clientScript.js
+++ clientScript.js
@@ -32,21 +32,12 @@ socket.on("user-total", (count) => {
   console.log(count);
   totalUsers.innerHTML = count;
 });
-
 socket.on("user-connected", (name) => {
   appendMessage(`${name} connected`, "userjoined");
 });
 
 socket.on("user-disconnected", (name) => {
   appendMessage(`${name} disconnected`, "disconnected");
-});
-
-messageForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const message = messageInput.value;
-  appendMessage(`You : ${message}`, "your-message");
-  socket.emit("send-chat-message", message);
-  messageInput.value = "";
 });
 
 function appendMessage(message, assignedClass) {
